2c80f72fcde6ebbce4cb2ac425ae746979175ee7
subin <subinshrestha2@bajratechnologies.com>
2022-07-03 20:44:05 +0545

    initial commit

---
+++ lib/data_fake.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "data_fake/version"
+
+module DataFake
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/data_fake/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module DataFake
+  VERSION = "0.1.0"
+end
